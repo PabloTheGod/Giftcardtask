0fb01addcf62a6ee0186bd2abc1bc13d0962d284
PabloTheGod <78507847+PabloTheGod@users.noreply.github.com>
2021-03-22 10:39:31 +1300

    Add files via upload

---
+++ yeah.py
@@ -0,0 +1,23 @@
+name = input("what is your name")
+print("hello " + name + " let's start")
+
+print("I'm gonna show you your result for 5 games")
+p_1 = 3
+print(p_1)
+
+p_2 = 6
+print(p_2)
+
+p_3 = 10
+print(p_3)
+
+p_4 = 10
+print(p_4)
+
+p_5 = 6
+print(p_5)
+
+
+print("now your total score")
+total = p_1 + p_2 + p_3 + p_4 + p_5
+print(total) 
